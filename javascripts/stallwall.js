3e072e32f72dd518f4e398606e545d8942a1746e
Abby Moore <abbyleighanne@gmail.com>
2016-10-27 15:09:44 -0500

    clear button

--- javascripts/stallwall.js
+++ javascripts/stallwall.js
@@ -1,13 +1,23 @@
 "use strict" 
 
-document.getElementById('input').addEventListener("keypress", function (e) {
+//default state
+document.getElementById("clear").disabled = true;
 
-	if (e.keyCode === 13) {
-		var userInput = document.getElementById('input').value; 
-		document.getElementById('output').innerHTML += "<p class='message'>" + userInput +"</p>";
-		console.log(userInput);
-		//still needs to run through the IIFE
+
+document.getElementById('input').addEventListener("keypress", function (e) {
+	var userInput = document.getElementById('input').value; 
+	
+	if (userInput !== "") {
+		document.getElementById("clear").disabled = false;
 	}
 
-})
+	if (e.keyCode === 13) {
+		document.getElementById('output').innerHTML += "<p class='message'>" + userInput +"</p>";
+	}
 
+});
+
+
+document.getElementById("clear").addEventListener("click", function erase() {
+		document.getElementById('output').innerHTML = "";
+});
