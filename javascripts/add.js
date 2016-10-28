590515af32b315203a0ee18ccfdc624b13466398
LaDonna <boxgirl85@gmail.com>
2016-10-28 14:58:40 -0500

    fixed delete btns and created add IIFE

--- javascripts/add.js
+++ javascripts/add.js
@@ -1,1 +1,22 @@
 "use strict"
+var addMsg = (function add(random){
+	var msgArray = [];
+
+	return {
+		storeMsg: function(msg){
+			var i = Math.floor((Math.random() * 100) + 1);
+        	var string = "<p class='msg' " + " 'id=p" + i + "'>" + msg + "<button class='btn'>" + "DELETE" + "</button>"  + "</p>";
+			msgArray += string;
+			var spot = document.getElementById("output");
+			spot.innerHTML += msgArray;
+			function getString(spot) {
+				touch(spot);
+			}
+		},
+
+		readMsg: function() {
+			return msgArray;
+		},
+
+	}
+	})();
