4f164067feb44aa09b088ec5ca538431d60011ea
Peter Staggs <peter.staggs@csuglobal.edu>
2016-10-31 09:26:25 -0500

    fixed clear all button, added logo, added css animations

--- javascripts/xhr.js
+++ javascripts/xhr.js
@@ -3,7 +3,7 @@
 function showData(returnedData){
 	//console.log(returnedData);
 	for (var i = 0; i < returnedData.length; i++){
-		document.getElementById("output").innerHTML += "<p class='msg'>" + returnedData[i] + "<button class='btn'>DELETE " + "</button>" + "</p>";
+		document.getElementById("output").innerHTML += "<p class='msg'>" + returnedData[i] + "<button class='btn'>SCRUB " + "</button>" + "</p>";
 	}
 
 };
